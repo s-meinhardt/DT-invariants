8424744d63266630de9d5497b2b91f83d26d2034
Sven Meinhardt <sven.meinhardt@posteo.de>
2023-04-18 22:27:51 +0100

    improve implementation of class Phase
    We implemented the following changes:
    1. Use the parent class 'Immutable' instead of dataclass to
       turn phases into immutable objects.
    2. Creating a phase from a complex number is now a class method
       rather then the initialization method.
    3. Initialize a phase from an integer is possible.
    4. Add hash method.
    5. Change representation of a phase.
    6. Turn methods without parameters into properties.
    7. Amend existing methods for the new changes.

--- app/dt_invariants/src/linear_algebra/phase.py
+++ app/dt_invariants/src/linear_algebra/phase.py
@@ -1,71 +1,117 @@
 from __future__ import annotations
 
 from cmath import phase
-from dataclasses import dataclass
 from math import pi
 
 
-@dataclass(init=True, frozen=True)
-class Phase:
-    """A phase represents a float phi satisfying
-    defined via z = |z| * exp(i * pi * phi)
-    for z = real + i * imag.
-    The 'branch' is related to the winding number
-    via winding number == branch // 2.
-    """
+from .immutable import Immutable
 
-    real: int
-    imag: int
-    branch: int
 
-    def __post_init__(self) -> None:
-        # validate input
-        if self.branch % 2:
-            assert self.imag < 0 or self.real > 0
-        else:
-            assert self.imag > 0 or self.real < 0
+class Phase(Immutable):
+    __slots__ = ["_real", "_imag", "branch"]
 
+    def __init__(self, value: int = 0) -> None:
+        assert isinstance(value, int), "Only integer values are allowed"
+
+        branch, remainder = divmod(value, 2)
+        object.__setattr__(self, "_real", (-1) ** remainder)
+        object.__setattr__(self, "_imag", 0)
+        object.__setattr__(self, "branch", branch)
+
+        super().__init__()
+
+    @classmethod
+    def of(cls, z: tuple[int, int], branch: int = 0) -> Phase:
+        phase = cls()
+        object.__setattr__(phase, "_real", z[0])
+        object.__setattr__(phase, "_imag", z[1])
+        object.__setattr__(phase, "branch", branch)
+        return phase
+
+    def covers(self, z: tuple[int, int]) -> bool:
+        return self == Phase.of(z, branch=self.branch)
+
+    @property
     def slope(self) -> float:
-        if self.imag:
-            return -self.real / self.imag
+        if self._imag:
+            return -self._real / self._imag
         else:
             return float("inf")
 
+    @property
+    def is_in_upper_half_plane(self) -> bool:
+        return self._imag > 0 or self._imag == 0 and self._real > 0
+
+    @property
+    def value(self) -> float:
+        return float(self)
+
+    def __repr__(self) -> str:
+        return f"Phase({float(self)})"
+
+    def __str__(self) -> str:
+        return repr(self)
+
+    def __hash__(self) -> int:
+        return hash((self._real, self._imag, self.branch))
+
     def __eq__(self, other: Phase) -> bool:  # type: ignore[override]
-        "'self' and 'other' have the same branch and slope"
-        return self.branch == other.branch and self.real * other.imag == other.real * self.imag
+        return (
+            int(self) == int(other) and self._real * other._imag == other._real * self._imag  # both have the same slope
+        )
 
     def __lt__(self, other: Phase) -> bool:
-        if self.branch < other.branch:
+        if int(self) < int(other):
             return True
-        elif self.branch > other.branch:
+        elif int(self) > int(other):
             return False
-        else:
-            return self.slope() < other.slope()
-
-    def covers(self, z: tuple[int, int]) -> bool:
-        """Checks if the complex number z[0] + i * z[1] has phase 'self' modulo 2
-
-        Args:
-            z (tuple[int, int]): a pair representing the real and imaginary part of a complex number
-
-        Returns:
-            bool: Has the complex number z[0] + i * z[1] phase 'self' modulo 2?
-        """
-        real, imag = z
-        if real or imag:
-            return self.real * imag == real * self.imag
-        else:
+        elif self._imag * other._real - self._real * other._imag < 0:
             return True
-
-    def __float__(self):
-        return self.branch + phase((-1) ** self.branch * complex(self.real, self.imag)) / pi
-
-    def __int__(self):
-        return self.branch if self.imag != 0 else self.branch + 1
-
-    def __getitem__(self, shift: int) -> Phase:
-        if shift % 2 == 0:
-            return Phase(real=self.real, imag=self.imag, branch=self.branch + shift)
         else:
-            return Phase(real=-self.real, imag=-self.imag, branch=self.branch + shift)
+            return False
+
+    def __le__(self, other: Phase) -> bool:
+        return self < other or self == other
+
+    def __float__(self) -> float:
+        return 2 * (self.branch + (self._imag < 0)) + phase(complex(self._real, self._imag)) / pi
+
+    def __int__(self) -> int:
+        if self.is_in_upper_half_plane:
+            return 2 * self.branch
+        else:
+            return 2 * self.branch + 1
+
+    def __bool__(self) -> bool:
+        return self.branch != 0 or self._imag != 0 or self._real != 0
+
+    def __add__(self, other: int | Phase) -> Phase:
+        if isinstance(other, int):
+            other = Phase(other)
+        if not isinstance(other, Phase):
+            ValueError("The second summand must be a Phase or an integer!")
+
+        real = self._real * other._real - self._imag * other._imag
+        imag = self._imag * other._real + self._real * other._imag
+        sum_is_in_upper_half_plane = imag > 0 or imag == 0 and real > 0
+        if self.is_in_upper_half_plane and other.is_in_upper_half_plane:
+            branch: int = self.branch + other.branch
+        elif self.is_in_upper_half_plane and not sum_is_in_upper_half_plane:
+            branch = self.branch + other.branch
+        elif other.is_in_upper_half_plane and not sum_is_in_upper_half_plane:
+            branch = self.branch + other.branch
+        else:
+            branch = self.branch + other.branch + 1
+        return Phase.of(z=(real, imag), branch=branch)
+
+    def __radd__(self, other: int) -> Phase:
+        return self + other
+
+    def __neg__(self) -> Phase:
+        return Phase.of(z=(self._real, -self._imag), branch=-self.branch - 1)
+
+    def __sub__(self, other: int | Phase) -> Phase:
+        return self + -other
+
+    def __rsub__(self, other: int) -> Phase:
+        return -self + other
