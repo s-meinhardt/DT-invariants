e7d525e920824486883a6928dda1381504b009ac
Sven Meinhardt <sven.meinhardt@posteo.de>
2023-04-21 08:28:56 +0100

    add documentation for 'dimension_vector'

--- app/dt_invariants/src/linear_algebra/dimension_vector.py
+++ app/dt_invariants/src/linear_algebra/dimension_vector.py
@@ -5,6 +5,116 @@ from .immutable import Immutable
 
 
 class DimensionVector(Immutable, Sequence):
+    """A class representing a tuple of integers.
+
+    A dimension vector is basically a tuple of integers.
+    The built-in Python tuple, however, does not behave
+    like a vector. Adding two such tuples concatenates
+    them rather than adding the coordinates.
+    This class represents a tuple but behaves like a vector.
+    Moreover, some useful extra properties and methods are
+    provided.
+    As the coordinates often represent dimensions, this class
+    is called 'dimension vector'.
+
+    Important
+    ---------
+    Dimension vectors are immutable objects. Thus, you cannot
+    change any of its coordinates after initialization, but
+    you can use dimension vectors as keys in a dictionary.
+
+    Parameters
+    ----------
+    args : Iterable[int]
+        The coordinates of the vector.
+
+    Attributes
+    ----------
+    is_zero: bool
+        Are all coordinates zero?
+
+    Examples
+    --------
+
+    You can initialize a dimension vector with a list of coordinates
+
+    >>> DimensionVector(1,2)
+    (1,2)
+
+    or by providing any iterable of integers such as a generator
+
+    >>> DimensionVector(x for x in range(3))
+    (0,1,2)
+
+    or a list.
+
+    >>> DimensionVector([1,4])
+    (1,4)
+
+    Is my dimension vector zero?
+
+    >>> DimensionVector(2,3,6).is_zero
+    False
+
+    If you need the zero dimension vector of a given length, use
+
+    >>> d = DimensionVector.zero(length=3)
+    >>> assert d.is_zero
+    >>> d
+    (0,0,0)
+
+    Use the copy method to create another copy of a dimension vector.
+
+    >>> d = DimensionVector(1,2)
+    >>> e = d.copy()
+    >>> d == e
+    True
+
+    >>> d is e
+    False
+
+    You can add or subtract dimension vectors as usual
+
+    >>> d + e
+    (2,4)
+
+    >>> d - e
+    (0,0)
+
+    or multiply them by integers.
+
+    >>> assert 3 * d == d * 3
+    >>> 3 * d
+    (3,6)
+
+    For dividing dimension vectors, you have 2 options:
+
+    1. Dividing through an integer performs a floor division
+       of the coordinates.
+
+       >>> d / 2
+       (0,1)
+
+    2. Floor division by another dimension vector which depends
+       on the choice of a 'positive' :class:`cone`.
+
+    When comparing dimension vectors, we use the lexicographic order
+    according to which we compare a specific coordinate starting from
+    the left and only move to the right neighbour in case of a tie.
+
+    >>> d = DimensionVector(1,4)
+    >>> e = DimensionVector(1,7)
+    >>> f = DimensionVector(2,1)
+    >>> assert d <= e
+    >>> assert d <= f
+
+    Note that this is a total order, i.e. any two dimension vectors
+    can be compared.
+    Another partial order will be introduced in the context of a
+    :class:`cone`.
+
+    """
+
     # __slots__ = ["_coord"]
 
     def __init__(self, *args) -> None:
@@ -17,14 +127,58 @@ class DimensionVector(Immutable, Sequence):
 
     @classmethod
     def zero(cls, length: int) -> DimensionVector:
+        """A method generating the zero dimension vector of a given length.
+
+        Parameters
+        ----------
+        length : int
+            The length of the requested dimension vector.
+
+        Returns
+        -------
+        DimensionVector
+            The requested zero dimension vector.
+
+        Example
+        -------
+
+        >>> DimensionVector.zero(length=3)
+        (0,0,0)
+
+        """
         return DimensionVector(0 for _ in range(length))
 
     @property
     def is_zero(self) -> bool:
+        """Is this dimension vector zero?
+
+        Returns
+        -------
+        bool
+            Are all coordinates zero?
+        """
         return all(map(lambda x: x == 0, self))
 
     def copy(self) -> DimensionVector:
+        """A method to create a different copy of a dimension vector.
 
+        Returns
+        -------
+        DimensionVector
+            Another copy of the dimension vector.
+
+        Example
+        -------
+
+        >>> d = DimensionVector(1,2)
+        >>> e = d.copy()
+        >>> d == e
+        True
+
+        >>> d is e
+        False
+
+        """
         return DimensionVector(x for x in self)
 
     def __len__(self) -> int:
@@ -34,6 +188,18 @@ class DimensionVector(Immutable, Sequence):
         return self._coord[position]
 
     def __add__(self, other: DimensionVector) -> DimensionVector:
+        """A method to add dimension vectors.
+
+        Parameters
+        ----------
+        other : DimensionVector
+            The second summand.
+
+        Returns
+        -------
+        DimensionVector
+            The dimension vector containing the pairwise sums of the coordinates.
+        """
         assert len(self) == len(other), "Both dimension vectors must have the same length!"
         return DimensionVector(map(lambda x: x[0] + x[1], zip(self, other)))
 
@@ -41,6 +207,13 @@ class DimensionVector(Immutable, Sequence):
         return self + other
 
     def __neg__(self) -> DimensionVector:
+        """A method returning the negative of a dimension vector.
+
+        Returns
+        -------
+        DimensionVector
+            The dimension vector with the signs of the coordinates swapped.
+        """
         return DimensionVector(-x for x in self)
 
     def __sub__(self, other: DimensionVector) -> DimensionVector:
@@ -50,6 +223,18 @@ class DimensionVector(Immutable, Sequence):
         return self - other
 
     def __mul__(self, factor: int) -> DimensionVector:
+        """A method to multiply a dimension vector with an integer.
+
+        Parameters
+        ----------
+        factor : int
+            The second factor.
+
+        Returns
+        -------
+        DimensionVector
+            The dimension vector with all coordinates scaled by 'factor'.
+        """
         return DimensionVector(map(lambda x: factor * x, self))
 
     def __rmul__(self, factor: int) -> DimensionVector:
@@ -59,6 +244,19 @@ class DimensionVector(Immutable, Sequence):
         return self * factor
 
     def __truediv__(self, divisor: int) -> DimensionVector:
+        """A method to divide a dimension vector by an integer.
+
+        Parameters
+        ----------
+        divisor : int
+            The divisor.
+
+        Returns
+        -------
+        DimensionVector
+            The dimension vector obtained by performing a floor division
+            of all coordinates by the divisor.
+        """
         return DimensionVector(map(lambda x: x // divisor, self))
 
     def __floordiv__(self, other: DimensionVector) -> int:
---
+++ docs/dimension_vector.rst
@@ -0,0 +1,7 @@
+Dimension Vector
+================
+
+.. py:currentmodule:: dt_invariants
+
+.. autoclass:: DimensionVector
+   :members: copy, zero 
