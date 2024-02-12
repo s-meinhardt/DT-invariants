af3a8ea5f8ba4a52d19d24da70505a1876bab868
Sven Meinhardt <sven.meinhardt@posteo.de>
2024-02-12 22:58:58 +0000

    add validations to quiver class

--- app/dt_invariants/src/examples/quiver.py
+++ app/dt_invariants/src/examples/quiver.py
@@ -9,43 +9,76 @@ from ..motives.symbols import L
 
 
 class Quiver:
-    def __init__(self, num_vertices: int, arrow_matrix: dict[tuple[int, int], int], name: Optional[str] = None) -> None:
-        self.num_vertices = num_vertices
-        self.vertices: range = range(num_vertices)
-        self.arrow_matrix = arrow_matrix
-        self.name = name if name else repr(self)
-        self._reps: Optional[AbelianCategory] = None
+  def __init__(
+    self,
+    num_vertices: int,
+    arrow_matrix: dict[tuple[int, int], int],
+    name: Optional[str] = None,
+  ) -> None:
+    self.num_vertices = num_vertices
+    self.vertices: range = range(num_vertices)
+    self.arrow_matrix = arrow_matrix
+    self.name = name if name else repr(self)
+    self._reps: Optional[AbelianCategory] = None
 
-        self.hom: Pairing = Pairing(rank=num_vertices)  # the standard scalar product
-        self.ext: Pairing = Pairing(rank=num_vertices, coeff_matrix=arrow_matrix)
+    self._validate()
 
-        # Euler pairing
-        coeff_matrix: dict[tuple[int, int], int] = {
-            (source, target): -num_arrows for (source, target), num_arrows in arrow_matrix.items()
-        }
-        coeff_matrix.update({(i, i): 1 + coeff_matrix.get((i, i), 0) for i in self.vertices})
-        self.euler_pairing: Pairing = Pairing(
-            rank=num_vertices, coeff_matrix={k: v for k, v in coeff_matrix.items() if v != 0}
+    self.hom: Pairing = Pairing(rank=num_vertices)  # the standard scalar product
+    self.ext: Pairing = Pairing(rank=num_vertices, coeff_matrix=arrow_matrix)
+
+    # Euler pairing
+    coeff_matrix: dict[tuple[int, int], int] = {
+      (source, target): -num_arrows
+      for (source, target), num_arrows in arrow_matrix.items()
+    }
+    coeff_matrix.update(
+      {(i, i): 1 + coeff_matrix.get((i, i), 0) for i in self.vertices}
+    )
+    self.euler_pairing: Pairing = Pairing(
+      rank=num_vertices, coeff_matrix={k: v for k, v in coeff_matrix.items() if v != 0}
+    )
+
+  @property
+  def reps(self) -> AbelianCategory:
+    if self._reps:
+      return self._reps
+    else:
+      self._set_quiver_reps()
+      return cast(AbelianCategory, self._reps)
+
+  def _set_quiver_reps(self) -> None:
+    motive_of_objects: MotivicSeries = MotivicSeries(
+      cone=StandardCone(rank=self.num_vertices),
+      at_dv=lambda d: L ** self.ext(d, d) / GL(d),
+      name=f"Generating Series({self.name})",
+    )
+
+    self._reps = AbelianCategory(
+      motive_of_objects=motive_of_objects,
+      euler_pairing=self.euler_pairing,
+      name=f"{self.name}-Reps",
+    )
+
+  def _validate(self) -> None:
+    if not isinstance(self.num_vertices, int) or self.num_vertices < 1:
+      raise ValueError("The number over vertices must be a positive integer.")
+    if not isinstance(self.arrow_matrix, dict):
+      raise ValueError("The arrow_matrix must be of type dict[tuple[int, int], int].")
+    for key, value in self.arrow_matrix.items():
+      if not isinstance(key, tuple) or len(key) != 2:
+        raise ValueError(
+          "The keys of the arrow_matrix must be a tuple of two integers."
         )
-
-    @property
-    def reps(self) -> AbelianCategory:
-        if self._reps:
-            return self._reps
-        else:
-            self.set_quiver_reps()
-            return cast(AbelianCategory, self._reps)
-
-    def set_quiver_reps(self) -> None:
-        motive_of_objects: MotivicSeries = MotivicSeries(
-            cone=StandardCone(rank=self.num_vertices),
-            at_dv=lambda d: L ** self.ext(d, d) / GL(d),
-            name=f"Generating Series({self.name})",
+      if key[0] not in self.vertices and key[1] not in self.vertices:
+        raise ValueError(
+          f"The vertices of the quiver must be called 0,...,{self.num_vertices-1}."
         )
-
-        self._reps = AbelianCategory(
-            motive_of_objects=motive_of_objects, euler_pairing=self.euler_pairing, name=f"{self.name}-Reps"
+      if not isinstance(value, int) or value < 0:
+        raise ValueError(
+          "The number of arrows between two vertices must be non-negative integer."
         )
+    if not isinstance(self.name, str):
+      raise ValueError("The name of the quiver must be a string.")
 
 
 # antisymmetrized Euler pairing of quiver reps
